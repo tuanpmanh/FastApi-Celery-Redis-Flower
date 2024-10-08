186131bb571904242a6eb776cdd5fec805b37d6d
tuanpm <tuanpmanh@gmail.com>
2024-10-08 17:36:50 +0700

    a

--- celery_worker.py
+++ celery_worker.py
@@ -6,8 +6,8 @@ from celery.utils.log import get_task_logger
 
 celery = Celery(
     "tasks",
-    broker="redis://redis:6379/0",
-    backend="redis://redis:6379/0",
+    broker="redis://:foobar@redis:6379/0",
+    backend="redis://:foobar@redis:6379/0",
 )
 
 celery_log = get_task_logger(__name__)
@@ -15,7 +15,7 @@ celery_log = get_task_logger(__name__)
 
 @celery.task
 def send_email(email: str):
-    time.sleep(random.randint(1, 4))
+    time.sleep(random.randint(3, 7))
     celery_log.info("Email has been sent")
     return {
         "msg": f"Email has been sent to {email}",
--- flower.dockerfile
+++ flower.dockerfile
@@ -1,6 +1,6 @@
 FROM python:3.11-alpine
 WORKDIR /app
 RUN pip install celery[redis] flower
-ENV CELERY_BROKER_URL=redis://redis:6379/0
+ENV CELERY_BROKER_URL=redis://:foobar@redis:6379/0
 EXPOSE 5555
 CMD ["celery", "flower", "--broker=$CELERY_BROKER_URL", "--broker_api=$CELERY_BROKER_URL", "--address=0.0.0.0", "--basic-auth=test:test"]
